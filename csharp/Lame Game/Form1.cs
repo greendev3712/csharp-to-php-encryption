b7ce81eb4d086d742d70bd82d1039d9725660f40
greendev3712 <businessxfreeman@gmail.com>
2023-02-26 05:19:45 +0100

    complete

---
+++ csharp/Lame Game/Form1.cs
@@ -0,0 +1,66 @@
+﻿//
+// Copyright (c) 2011 Scott Clayton
+//
+// This file is part of the C# to PHP Encryption Library.
+//
+// The C# to PHP Encryption Library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The C# to PHP Encryption Library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the C# to PHP Encryption Library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using CS2PHPCryptography;
+using System;
+using System.Windows.Forms;
+
+namespace Lame_Game
+{
+    public partial class Form1 : Form
+    {
+        private SecurePHPConnection secure;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            secure = new SecurePHPConnection();
+            secure.OnConnectionEstablished += new SecurePHPConnection.ConnectionEstablishedHandler(secure_OnConnectionEstablished);
+            secure.OnResponseReceived += new SecurePHPConnection.ResponseReceivedHandler(secure_OnResponseReceived);
+
+            secure.SetRemotePhpScriptLocation("http://skot2/enc/score.php");
+            secure.EstablishSecureConnectionAsync();
+
+            button1.Enabled = false;
+        }
+
+        private void secure_OnResponseReceived(object sender, ResponseReceivedEventArgs e)
+        {
+            MessageBox.Show(e.Response);
+        }
+
+        private void secure_OnConnectionEstablished(object sender, OnConnectionEstablishedEventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            decimal score = (int)numericUpDown1.Value;
+            string message = name + "," + score.ToString();
+
+            if (secure.OKToSendMessage)
+            {
+                secure.SendMessageSecureAsync(message);
+            }
+        }
+    }
+}
---
+++ helpers/php-safe-key/PhpSafeKey/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PhpSafeKey
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ofdPrivateKey = new System.Windows.Forms.OpenFileDialog();
+            this.sfdPrivateKey = new System.Windows.Forms.SaveFileDialog();
+            this.timerPopup = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ofdPrivateKey
+            // 
+            this.ofdPrivateKey.FileName = "private.key";
+            this.ofdPrivateKey.Filter = "Private Key Files|*.key";
+            this.ofdPrivateKey.Title = "Select a private key to encode into a PHP file.";
+            // 
+            // sfdPrivateKey
+            // 
+            this.sfdPrivateKey.DefaultExt = "*.php";
+            this.sfdPrivateKey.FileName = "private.php";
+            this.sfdPrivateKey.Filter = "PHP Files|*.php";
+            this.sfdPrivateKey.Title = "Save the PHP encoded private key.";
+            // 
+            // timerPopup
+            // 
+            this.timerPopup.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(10, 10);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "PHP Safe Key";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdPrivateKey;
+        private System.Windows.Forms.SaveFileDialog sfdPrivateKey;
+        private System.Windows.Forms.Timer timerPopup;
+    }
+}
+
